54ba2fc11f80357dc99331d51ad73005ff39d081
jordankitio <83547629+jordankitio@users.noreply.github.com>
2023-12-06 22:05:53 -0600

    Update main.js
    added Pin to searched address return

--- main.js
+++ main.js
@@ -1,61 +1,49 @@
-
-
-// Amazon Location Service resource names:
+// Amazon Location Service resource names
 const mapName = "virtualthingmap";
 const placesName = "virtualthings_placeindex";
 const region = "US-East-1";
-const apiKey = "v1.public.eyJqdGkiOiJhODA4NWY5ZS1jOTFiLTQ4MWUtYTU0My0yYTJjZTQxNmFjM2YifVPL73W6FyAf1rN8BLCM34FXc5Z3EJ_cHV_H4fgoIdDaQmpTLqjlyz90QU1DCXKOPWSzvgA63S2DmKGUFo9nmcLrRhjmp5VM7u9aQeYBAkKzARdUQ5-mHdpMKRngcbHHV3_If-vIq9t6uLIW9FswU38V2ClfX5QQELOWcOA5-zXW8kE8jlxxx8J0oVQoUBfNrSa0san1_ZEclE0Bz5Q4zI2ZsQp6A2vo2awrZI-mUXvaJwI7a_h5AG0SR8tW74_Lpvgkbe7AeZDvsAzy4Wnn_wbI-tx087G56zoux7ZLS7jsJ_2NNwk77CeEvbGKCAWmHoOrPk7_EP_x5veBXxs1T78.ZWU0ZWIzMTktMWRhNi00Mzg0LTllMzYtNzlmMDU3MjRmYTkx"
-const Tracker ="Virtualthing1_tracker";
-// CREATE THE MAP IN THE SPACE YOU PROVIDED IN YOUR HTML CODE (<div id=map> </div>)
+const apiKey = "v1.public.eyJqdGkiOiJhODA4NWY5ZS1jOTFiLTQ4MWUtYTU0My0yYTJjZTQxNmFjM2YifVPL73W6FyAf1rN8BLCM34FXc5Z3EJ_cHV_H4fgoIdDaQmpTLqjlyz90QU1DCXKOPWSzvgA63S2DmKGUFo9nmcLrRhjmp5VM7u9aQeYBAkKzARdUQ5-mHdpMKRngcbHHV3_If-vIq9t6uLIW9FswU38V2ClfX5QQELOWcOA5-zXW8kE8jlxxx8J0oVQoUBfNrSa0san1_ZEclE0Bz5Q4zI2ZsQp6A2vo2awrZI-mUXvaJwI7a_h5AG0SR8tW74_Lpvgkbe7AeZDvsAzy4Wnn_wbI-tx087G56zoux7ZLS7jsJ_2NNwk77CeEvbGKCAWmHoOrPk7_EP_x5veBXxs1T78.ZWU0ZWIzMTktMWRhNi00Mzg0LTllMzYtNzlmMDU3MjRmYTkx";
+const Tracker = "Virtualthing1_tracker";
+
+// Initialize map in the specified HTML element
 async function initializeMap() {
-  let mlglMap; // declare a constant to name the map
+  let mlglMap;
   mlglMap = new maplibregl.Map({
     container: "map", // HTML element ID of map element
     center: [-102.13332, 41.0189], // Initial map centerpoint
     zoom: 2, // Initial map zoom
     style: `https://maps.geo.${region}.amazonaws.com/maps/v0/maps/${mapName}/style-descriptor?key=${apiKey}`, // Defines the appearance of the map and authenticates using an API key
   });
+
   // Add navigation control to the top left of the map
   mlglMap.addControl(new maplibregl.NavigationControl(), "top-left");
 
-
-
   return mlglMap; // DONE
-  }
+}
 
-
-
-
-
-
-
-// the main function is where the execution of the code starts. start the map initialization process.
-// it's inside the main function that you'll add your map functionalities
-
+// Main function where the execution starts
 async function main() {
   // Create an authentication helper instance using an API key
   let authHelper;
   authHelper = await amazonLocationAuthHelper.withAPIKey(apiKey);
 
-
-  // Initialize map and Amazon Location SDK client:
+  // Initialize map and Amazon Location SDK client
   const map = await initializeMap();
   map.addControl(
-        new maplibregl.GeolocateControl({
-            positionOptions: {
-                enableHighAccuracy: true
-            },
-            trackUserLocation: true
-        }));
+    new maplibregl.GeolocateControl({
+      positionOptions: {
+        enableHighAccuracy: true
+      },
+      trackUserLocation: true
+    })
+  );
 
   const client = new amazonLocationClient.LocationClient({
     region,
     ...authHelper.getLocationClientConfig(), // Provides configuration required to make requests to Amazon Location
   });
 
-
-
-  // On mouse click, display marker and get results:
+  // On mouse click, display marker and get results
   map.on("click", async function (e) {
     // Set up parameters for search call
     let params = {
@@ -63,27 +51,24 @@ async function main() {
       Position: [e.lngLat.lng, e.lngLat.lat],
       Language: "en",
       MaxResults: "5",
-
     };
 
+    // Add marker when clicked
     {
       let marker = new maplibregl.Marker()
-  .setLngLat(params.Position)
-  .addTo(map);
+        .setLngLat(params.Position)
+        .addTo(map);
 
-      setTimeout(() => {      //ADDS MARKER WHEN YOU CLICK
+      setTimeout(() => {
         marker.remove();
-      }, 300)
-
-    };
-
+      }, 300);
+    }
 
     // Set up command to search for results around clicked point
     let searchCommand;
     searchCommand = new amazonLocationClient.SearchPlaceIndexForPositionCommand(params);
 
     try {
-
       // Make request to search for results around clicked point
       let data;
       data = await client.send(searchCommand);
@@ -93,37 +78,26 @@ async function main() {
 
       // Display place label in an alert box
       alert(data.Results[0].Place.Label);
-
-
-
-     }
-     catch (error) {
+    } catch (error) {
       // Write JSON response error to HTML
       document.querySelector("#response").textContent = JSON.stringify(error, undefined, 2);
 
       // Display error in an alert box
       alert("There was an error searching.");
     }
-
-
-
   });
 
+  // Tracker
 
-
-///TRACKER
-
-
-
-// Function to zoom to a specific location
+  // Function to zoom to a specific location
   function zoomToLocation(lat, lng, zoom) {
     map.flyTo({
       center: [lng, lat],
       zoom: zoom,
     });
-  };
+  }
 
-  // Handle search button click (Added Part)
+  // Handle search button click
   document.querySelector("#search-button").addEventListener("click", async () => {
     const placeQuery = document.querySelector("#place-query").value;
 
@@ -147,6 +121,25 @@ async function main() {
         const coordinates = result.Place.Geometry.Point;
         zoomToLocation(coordinates[1], coordinates[0], 5);
 
+        // Add a marker for the searched location
+        const marker = new maplibregl.Marker()
+        .setLngLat(coordinates)
+        .addTo(map);
+
+        // waits 5 secs to disappear
+        const timeoutId = setTimeout(() => {
+         marker.remove();
+         }, 5000);
+
+        // Listen for a map click to remove the marker and clear the timeout
+         map.once("click", () => {
+           marker.remove();
+           clearTimeout(timeoutId);
+         });
+
+
+
+
         // Write JSON response data to HTML
         document.querySelector("#response").textContent = JSON.stringify(data, undefined, 2);
       } else {
@@ -160,13 +153,6 @@ async function main() {
       alert("There was an error searching.");
     }
   });
-
-
-
-
-
-
 }
+
 main();
-
-
